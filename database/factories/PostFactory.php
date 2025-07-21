7e0eeddb82c614c391c458d9cfbf32c79702d992
natalie ng <nwy0206@gmail.com>
2025-07-21 00:55:57 -0700

    Create post model with migration and factory

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(),
+            'slug' => $this->faker->slug(),
+            'content' => $this->faker->paragraph(),
+            'published_at' => $this->faker->date(),
+            'user_id' => User::all()->random()->id,
+            'image' => $this->faker->imageUrl(),
+        ];
+    }
+}
